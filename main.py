5c2581a1c3a80319390b6647a4af84f419411dec
Mikhail.Yakushkin <Mikhail.Yakushkin@optimum-is.com>
2023-07-22 21:23:48 +0300

    listen two networks

--- main.py
+++ main.py
@@ -7,31 +7,27 @@ import threading
 import queue
 from datetime import datetime
 import asyncio
-from info import infura_goerli_url, goerli_ms_sc_adr, goerli_ms_sc_abi
+from info import infura_goerli_url, goerli_ms_sc_adr, goerli_ms_sc_abi, \
+    sibr_net_url, sibr_ms_sc_adr, sibr_ms_sc_abi, abi_of_sc, provider_of_sc
 
 guradian_adr = '0xc6322A3D73f791dFf977984F5380468885Beed77'
 guradian_adr_pk = '540884f10d684620077d6b90a65c1a256f3f00172df5635d73c99178c45950af'
 
 message_queue = queue.Queue()
 
+eth_web3 = Web3(Web3.HTTPProvider(infura_goerli_url, request_kwargs={'timeout': 60}))
+eth_contract = eth_web3.eth.contract(address=goerli_ms_sc_adr, abi=goerli_ms_sc_abi)
+eth_event_filters = (eth_contract.events.IssueInited.create_filter(fromBlock='latest'))
+
+sibr_web3 = Web3(Web3.HTTPProvider(sibr_net_url, request_kwargs={'timeout': 60}))
+sibr_contract = sibr_web3.eth.contract(address=sibr_ms_sc_adr, abi=sibr_ms_sc_abi)
+sibr_event_filters = (sibr_contract.events.IssueInited.create_filter(fromBlock='latest'))
+
 
 def standard_trx_build_for_sc_call_with_gas(base_adr, provider_url: str) -> dict:
     if base_adr is None:
         return None
     web3 = Web3(Web3.HTTPProvider(provider_url, request_kwargs={'timeout': 60}))
-
-    # build_trx_config = {
-    #     'chainId': web3.eth.chain_id,
-    #     'from': base_adr,
-    #     'gasPrice': web3.eth.gas_price,
-    #     'nonce': web3.eth.get_transaction_count(base_adr)
-    # }
-    # gas_eddition = 50000
-    # if not provider_url.index("infura"):
-    #     gas_eddition = 100000
-    # gas = web3.eth.estimate_gas(build_trx_config) + gas_eddition
-    # build_trx_config['gas'] = gas + int(gas * 0.2)
-    # build_trx_config['gasPrice'] += int(build_trx_config['gasPrice'] * 0.2)
 
     build_trx_config = {
         'chainId': web3.eth.chain_id,
@@ -74,11 +70,11 @@ def handle_event(event):
         to_address = event_log_receipt.get('args').get('to')
         value_wei = event_log_receipt.get('args').get('value')
         issue_index = event_log_receipt.get('args').get('issueIndex')
-        trx_hash = event_log_receipt.get('transactionHash', HexBytes('0x0000')).hex()
+        #trx_hash = event_log_receipt.get('transactionHash', HexBytes('0x0000')).hex()
         message_queue.put(
             {'type': 'handle_issue_inited',
              'sc_address': address_of_sc,
-             'tx_init_hash': trx_hash,
+             #'tx_init_hash': trx_hash,
              'to_address': to_address,
              'value': value_wei,
              'issue_index': issue_index})
@@ -92,18 +88,32 @@ def read_queue_messages(message_queue):
         print(f"Received message: {message} (Timestamp: {timestamp})")
         m_type = message.get('type', '')
         if m_type == 'handle_issue_inited':
-            time.sleep(3)
             m_id_in_contract = int(message.get('issue_index'))
+            m_sc_adr = message.get('sc_address')
             signIssueInMSW(
                 guardian_adr=guradian_adr,
                 guardian_adr_pk=guradian_adr_pk,
                 issue_id=m_id_in_contract,
-                sc_address=goerli_ms_sc_adr,
-                sc_abi=goerli_ms_sc_abi,
-                provider_url=infura_goerli_url)
+                sc_address=m_sc_adr,
+                sc_abi=abi_of_sc[m_sc_adr],
+                provider_url=provider_of_sc[m_sc_adr])
 
 
 async def eth_events_handler(event_filters, poll_interval):
+    while True:
+        if type(event_filters) is LogFilter:
+            for SomeEventArrived in event_filters.get_new_entries():
+                await asyncio.sleep(poll_interval*2)
+                handle_event(SomeEventArrived)
+            await asyncio.sleep(poll_interval)
+        else:
+            for event_filter in event_filters:
+                for SomeEventArrived in event_filter.get_new_entries():
+                    handle_event(SomeEventArrived)
+                await asyncio.sleep(poll_interval)
+
+
+async def sibr_events_handler(event_filters, poll_interval):
     while True:
         if type(event_filters) is LogFilter:
             for SomeEventArrived in event_filters.get_new_entries():
@@ -114,6 +124,7 @@ async def eth_events_handler(event_filters, poll_interval):
                 for SomeEventArrived in event_filter.get_new_entries():
                     handle_event(SomeEventArrived)
                 await asyncio.sleep(poll_interval)
+
 
 
 def listen_eth_events():
@@ -136,8 +147,29 @@ def listen_eth_events():
         loop.close()
 
 
+def listen_eth_and_sibr_events():
+    #asyncio.create_task(eth_events_handler(eth_event_filters, 1))
+    #asyncio.create_task(sibr_events_handler(sibr_event_filters, 1))
+
+    # Start a separate thread to read messages from the queue
+    queue_thread = threading.Thread(target=read_queue_messages, args=(message_queue,))
+    queue_thread.start()
+
+    loop = asyncio.get_event_loop()
+    try:
+        loop.run_until_complete(
+            asyncio.gather(
+                eth_events_handler(eth_event_filters, 1),
+                sibr_events_handler(sibr_event_filters, 1)
+            ))
+    finally:
+        # close loop to free up system resources
+        loop.close()
+
+
 def start_guardian_checker():
-    listen_eth_events()
+    #listen_eth_events()
+    listen_eth_and_sibr_events()
 
 
 # Press the green button in the gutter to run the script.
